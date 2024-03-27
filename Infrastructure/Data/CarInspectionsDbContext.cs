e6f0112f47978bca633fd57c475c7a67fbe35475
eaygun <enes123454@hotmail.com>
2024-03-27 15:28:23 +0100

    Add project files.

---
+++ Infrastructure/Data/CarInspectionsDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class CarInspectionsDbContext : DbContext
+    {
+        public CarInspectionsDbContext(DbContextOptions<CarInspectionsDbContext> options) : base(options) { }
+    }
+}
